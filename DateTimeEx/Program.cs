3aca6501cddbdc09f6dd3b4aa43b98f3171cedae
Arieleon25 <Arieleon25@yahoo.com>
2023-03-22 17:56:28 -0400

    Add project files.

---
+++ DateTimeEx/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timenow = DateTime.Now;
+            Console.WriteLine(timenow);
+
+            Console.WriteLine("Please input a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int[] hours = { userInput };
+
+            foreach (int input in hours)
+            {
+                Console.WriteLine("{0} + {1} hour(s) = {2}", timenow, input, timenow.AddHours(input));
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
